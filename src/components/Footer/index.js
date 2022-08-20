ed23f4e09a655c8b8df4480006d81d41522b9391
roshan-acharya <roshanacharya9808@gmail.com>
2022-08-20 11:51:11 +0545

    footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,13 @@
 import styles from "./footer.module.css";
 
 const Footer = () => {
-  return <footer className={styles.footer}>Footer</footer>;
+  return <footer className={styles.footer}>
+    <div>
+      <h1 className={styles.thanks}>
+        Thanks to <a href="https://redis.com/">Redis</a> X <a href="https://dev.to/">dev.to</a>
+      </h1>
+    </div>
+  </footer>;
 };
 
 export default Footer;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,7 +1,16 @@
-import styles from "./style.module.css";
-
+import styles from "./style.module.css"
+import { Link } from 'react-router-dom'
 const Navbar = () => {
-  return <div className={styles.container}>Hi this is a Navbar</div>;
+  return <div className={styles.container}>
+    <div className={styles.contain}>
+    <Link to="/searchbox">
+     <button className={styles.button}>
+        <p>Webportal</p>
+     </button>
+ </Link>
+    </div>
+
+  </div>;
 };
 
 export default Navbar;
