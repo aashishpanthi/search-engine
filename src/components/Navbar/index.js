b4df6390dc7a11e845ffeb568b29b634f827716c
roshan-acharya <roshanacharya9808@gmail.com>
2022-08-20 14:33:10 +0545

    dark mode something ready

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,16 +1,36 @@
 import styles from "./style.module.css"
+import { useEffect} from "react";
+import { useState } from "react";
 import { Link } from 'react-router-dom'
 const Navbar = () => {
-  return <div className={styles.container}>
+  const [theme, setTheme] = useState('light-theme')
+  const toggleTheme=()=>{
+    if(theme==='dark-theme'){
+      setTheme('light-theme')
+    }
+    else{
+      setTheme('dark-theme')
+    }
+  };
+  useEffect(()=>{
+    document.body.className=theme;
+  },[theme]);
+  return (
+  <div className={styles.container}>
     <div className={styles.contain}>
-    <Link to="/searchbox">
+    <Link to="/add">
      <button className={styles.button}>
         <p>Webportal</p>
      </button>
  </Link>
     </div>
-
-  </div>;
+<div>
+  <button className={styles.buttonn} onClick={()=>toggleTheme()}>
+    Enable dark mode
+  </button>
+</div>
+  </div>
+  );
 };
 
 export default Navbar;
