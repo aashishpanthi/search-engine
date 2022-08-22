0f2950d8d6c2d1d1b90188f0a101404cfb24f1f6
roshan-acharya <roshanacharya9808@gmail.com>
2022-08-23 01:06:22 +0545

    web

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,15 +1,42 @@
 import styles from "./style.module.css"
 import { useEffect} from "react";
 import { useState } from "react";
-import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import * as React from 'react';
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import CloseIcon from '@mui/icons-material/Close';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  boxShadow: 24,
+  p: 4,
+};
 const Navbar = () => {
-  const [theme, setTheme] = useState('light-theme')
+  //mui 
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  //theme
+  const [box,setBox]=useState('fa-solid fa-moon');
+  const [theme, setTheme] = useState('light-theme');
   const toggleTheme=()=>{
     if(theme==='dark-theme'){
       setTheme('light-theme')
+      setBox("fa-solid fa-moon")
     }
     else{
       setTheme('dark-theme')
+      setBox('fa-solid fa-sun')
     }
   };
   useEffect(()=>{
@@ -18,17 +45,39 @@ const Navbar = () => {
   return (
   <div className={styles.container}>
     <div className={styles.contain}>
-    <Link to="/add">
-     <button className={styles.button}>
+     <button className={styles.button} onClick={handleOpen}>
         <p>Webportal</p>
      </button>
- </Link>
-    </div>
-<div>
-  <button className={styles.buttonn} onClick={()=>toggleTheme()}>
-    Enable dark mode
+ 
+ <button className={styles.buttonn} onClick={()=>toggleTheme()}>
+  <FontAwesomeIcon icon={box} className={styles.icon} />
   </button>
-</div>
+    </div>
+    <div>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+            <Button onClick={()=>handleClose()}><CloseIcon/></Button>
+            <Typography id="transition-modal-title" variant="h6" component="h2">
+              Webportal
+            </Typography>
+            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+              Let us know about your sites
+            </Typography>
+          </Box>
+        </Fade>
+      </Modal>
+    </div>
   </div>
   );
 };
