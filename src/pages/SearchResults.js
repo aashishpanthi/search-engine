bcf60ba9b1732bb2cf64e5b6ea44746fce733f53
Aashish Panthi <aashishpanthi11@gmail.com>
2022-08-23 14:30:47 +0545

    search-result styles modified

--- src/pages/SearchResults.js
+++ src/pages/SearchResults.js
@@ -2,21 +2,21 @@ import React from "react";
 import styles from "./styles/searchresult.module.css";
 import { Content } from "../components";
 const SearchResults = () => {
-  return <div className={styles.container}>
-    <div className={styles.sidenav}>
-      <button className={styles.btn}>All</button>
-      <button className={styles.btn}>Images</button>
-      <button className={styles.btn}>Videos</button>
+  return (
+    <div className={styles.container}>
+      <div className={styles.sidenav}>
+        <button className={styles.btn}>All</button>
+        <button className={styles.btn}>Images</button>
+        <button className={styles.btn}>Videos</button>
+      </div>
+      <div className={styles.result}>
+        <Content />
+        <Content />
+        <Content />
+        <Content />
+      </div>
     </div>
-    <div className={styles.result}>
-    <Content/>
-    <Content/>
-    <Content/>
-    <Content/>
- 
-    </div>
-  
-  </div>
+  );
 };
 
 export default SearchResults;
