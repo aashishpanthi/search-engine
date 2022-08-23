917bfd184dc0b14e0dc7fde97e766f69e3b0fc93
Aashish Panthi <aashishpanthi11@gmail.com>
2022-08-23 14:40:25 +0545

    search box style improved

--- src/components/Searchbox/index.js
+++ src/components/Searchbox/index.js
@@ -1,24 +1,23 @@
 import styles from "./style.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 const SearchBox = () => {
   return (
-    <div className={styles.search_box}>
+    <div className={styles.search_box} id="searchBox">
       <input
         type="text "
         placeholder="Search anything..."
         className={styles.search_input}
       ></input>
-      
+
       <div className={styles.icon}>
-      <Link to='/search'>
-        <FontAwesomeIcon
-          icon="fa-solid fa-magnifying-glass"
-          className={styles.search_icon}
-        />
+        <Link to="/search">
+          <FontAwesomeIcon
+            icon="fa-solid fa-magnifying-glass"
+            className={styles.search_icon}
+          />
         </Link>
       </div>
-      
     </div>
   );
 };
