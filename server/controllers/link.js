b01609a972b5db9a0cbfcd85bb760584f39abfb6
Aashish Panthi <aashishpanthi11@gmail.com>
2022-08-28 09:31:59 +0545

    validate links and save it

--- server/controllers/link.js
+++ server/controllers/link.js
@@ -2,14 +2,20 @@ import Link from "../models/links.js";
 import Joi from "joi";
 
 export const addLink = async (req, res, next) => {
+  const url = new URL(req.body.url);
+  const baseUrl = `${url.protocol}//${url.hostname}${url.pathname}`;
+
   const schema = Joi.object({
     url: Joi.string().required(),
   });
 
-  const { value, error } = schema.validate(req.body);
+  const { value, error } = schema.validate({ url: baseUrl });
   if (error) return res.status(400).json({ error: error.details, ...value });
 
-  const link = new Link(value);
+  const link = new Link({
+    ...value,
+    lastUpdated: new Date(new Date().setMonth(new Date().getMonth() - 1)),
+  });
   try {
     await link.save((err, s) => {
       res.status(200).json({ success: true, item: s });
