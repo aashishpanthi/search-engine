58ad786331ce275faeaaee1b1f1a11870ab70b54
Aashish Panthi <aashishpanthi11@gmail.com>
2022-08-29 00:29:34 +0545

    pwa setup

--- client/src/index.js
+++ client/src/index.js
@@ -7,10 +7,9 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-  <App/>
+    <App />
   </BrowserRouter>
- 
 );
 
 // PWA
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
