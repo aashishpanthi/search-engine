e9f0a475035e5b28140da69a59bdbc068bc0736d
Aashish Panthi <aashishpanthi11@gmail.com>
2022-08-29 13:21:01 +0545

    import path

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./server/config/mongoDb.js";
+import path from "path";
 
 //router imports
 import searchRoutes from "./server/routes/search.js";
